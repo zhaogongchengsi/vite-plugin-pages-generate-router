55cc89b6ed9dd44a4bdc101679cdd05c4f8d49ed
zhaogongchengsi <zzh1586169624@163.com>
2023-01-11 15:20:35 +0800

    fix: Eslint failure

--- examples/vue/src/router/index.ts
+++ examples/vue/src/router/index.ts
@@ -1,26 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import pageRouters from 'page-router'
-import type { App } from 'vue'
-import { asyncComponents } from './utils'
-import RootView from '@/views/index.vue'
+import { createRouter, createWebHistory } from "vue-router";
+// @ts-expect-error
+import pageRouters from "page-router";
+import type { App } from "vue";
+import { asyncComponents } from "./utils";
+import RootView from "@/views/index.vue";
 
-// @ts-expect-error
-// @ts-expect-error
-const modules = import.meta.glob('../views/**/*.vue')
+const modules = import.meta.glob("../views/**/*.vue");
 
 export function createAppRouter(app: App) {
-  const routers = asyncComponents(pageRouters, modules)
+  const routers = asyncComponents(pageRouters, modules);
   const router = createRouter({
-    // @ts-expect-error
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
       {
         component: RootView,
-        path: '/',
-        name: 'root',
+        path: "/",
+        name: "root",
       },
       ...routers,
     ],
-  })
-  app.use(router)
+  });
+  app.use(router);
 }
--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -1,21 +1,20 @@
-import { URL, fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { URL, fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 // @ts-expect-error
-import { pageGenerateRouter } from '../../dist/index.mjs'
+import { pageGenerateRouter } from "../../dist/index.mjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     pageGenerateRouter({
-      generateDir: './src/views',
+      generateDir: "./src/views",
     }),
   ],
   resolve: {
     alias: {
-      // @ts-expect-error
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
